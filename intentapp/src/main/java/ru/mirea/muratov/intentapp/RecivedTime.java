7e503980b9244ec8dec4b78bd4f865f2eea0fc10
Trilgon <65063208+Trilgon@users.noreply.github.com>
2021-04-10 18:34:32 +0300

    Initial commit

---
+++ intentapp/src/main/java/ru/mirea/muratov/intentapp/RecivedTime.java
@@ -0,0 +1,20 @@
+package ru.mirea.muratov.intentapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class RecivedTime extends AppCompatActivity {
+    TextView textViewTime;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recived_time);
+        textViewTime = findViewById(R.id.TextTime);
+        Intent intent = getIntent();
+        textViewTime.setText(intent.getStringExtra("time"));
+    }
+}
---
+++ resultapp/src/main/java/ru/mirea/muratov/resultapp/InfActivity.java
@@ -0,0 +1,26 @@
+package ru.mirea.muratov.resultapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class InfActivity extends AppCompatActivity {
+    EditText editTextTitle;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_inf);
+        editTextTitle = findViewById(R.id.edit_title);
+    }
+
+    public void sendResultOnMainActivityOnClick(View view) {
+        Intent intent = new Intent();
+        intent.putExtra("name", editTextTitle.getText().toString());
+        setResult(RESULT_OK, intent);
+        finish();
+    }
+}
